45aaa01aa048e100879b6b928952ca3f364d7b4a
Rafael Conrado <106274045+RafaelCGConrado@users.noreply.github.com>
2022-09-25 20:33:42 -0300

    Update funcoes.h

--- funcoes.h
+++ funcoes.h
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
+struct vinhos{
+    int id;
+    double citric_acid, residual_sugar, 
+    density, pH, alcohol;
+};
+
 typedef struct vinhos VINHO;
 
 char *readline(FILE *stream);
-//Lê uma linha inteira de entrada e retorna a string
+//FUNÇÃO UTILIZADA PARA LER LINHA A LINHA DO ARQUIVO
 
 VINHO *cria_vetor();
 //cria vetor do tipo VINHO
@@ -11,3 +17,11 @@ VINHO *cria_vetor();
 char *recorta_string(char *str, char *delimitador);
 
 void ordena(VINHO *p, int tamanho);
+
+void readfile(FILE *stream);
+
+int conta_vinhos(FILE *stream);
+//CONTA A QUANTIDADE DE VINHOS PRESENTES NO ARQUIVO
+
+// void ordena(VINHO *arr);
+    //prototipo

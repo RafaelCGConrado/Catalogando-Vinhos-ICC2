893e0c4229c6e7631a8a0a5909e5e6fad4807664
Rafael Conrado <106274045+RafaelCGConrado@users.noreply.github.com>
2022-09-28 18:00:13 -0300

    Update funcoes.c

--- funcoes.c
+++ funcoes.c
@@ -1,6 +1,70 @@
 #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h> 
+#include <string.h>
+#include "funcoes.h"
+
+
+int main(){
+    FILE *arq = fopen("vinho.csv", "r");
+    VINHO *arr = cria_vetor();
+    
+    
+
+    int tam_vetor = 0;
+
+    //leitura do arquivo OBS TRANSFORMAR EM FUNÇÃO URGENTEMENTE
+    char *str = readline(arq);
+    char *token;
+    while(!feof(arq)){
+        
+        str = readline(arq);
+        
+        token = strtok(str, ",");
+        arr[tam_vetor].id = atoi(token);
+
+        token = strtok(NULL, ",");
+        arr[tam_vetor].citric_acid = atof(token);
+
+        token = strtok(NULL, ",");
+        arr[tam_vetor].residual_sugar = atof(token);
+        
+        token = strtok(NULL, ",");
+        arr[tam_vetor].density = atof(token);
+        
+        token = strtok(NULL, ",");
+        arr[tam_vetor].pH = atof(token);
+
+        
+        token = strtok(NULL, ",");
+        arr[tam_vetor].alcohol = atof(token);
+
+
+        tam_vetor++;
+
+        arr = realloc(arr, (tam_vetor + 1) * sizeof(VINHO));
+
+    }
+    fclose(arq);
+
+    
+    char caracteristica[20];
+    int i = compara_caracteristica(caracteristica);
+    printf("%d", i);
+
+    
+
+
+    //PROXIMO PASSO: ORDENAR ESSA BAGAÇA
+
+
+    
+    free(arr);
+
+    return 0;
+
+    
+}   #include <string.h> 
 #include "funcoes.h"
 
 
